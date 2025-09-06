3b70a24b6787667bbf17290fedb024c2dabf47ba
danifer24 <danielvf24@gmail.com>
2025-09-06 20:06:20 +0200

    Agregar reducer para manejar la adición de usuarios y actualizar el estado en UsersApp

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,5 +1,7 @@
+import { useReducer } from "react";
 import { UserForm } from "./components/UserForm";
 import { UsersList } from "./components/UsersList";
+import { usersReducer } from "./reducers/usersReducer";
 
 const initialUsers = [
     {
@@ -11,8 +13,14 @@ const initialUsers = [
 ];
 export const UsersApp = () => {
 
+    const [users, dispatch] = useReducer(usersReducer, initialUsers);
+
     const handlerAddUser = (user) => {
-        console.log(user);
+        // console.log(user);
+        dispatch({
+            type: 'addUser',
+            payload: user
+        })
     }
 
     return (
@@ -24,7 +32,7 @@ export const UsersApp = () => {
                 </div>
                 <div className="col">
                     <UsersList
-                        users={initialUsers} />
+                        users={users} />
                 </div>
             </div>
         </div>
