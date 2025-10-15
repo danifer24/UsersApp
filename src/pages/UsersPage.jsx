4e57df2c24cb7ab4acd1229384ac113319f8e415
danifer24 <danielvf24@gmail.com>
2025-10-15 02:31:36 +0200

    Agregar validación y manejo de errores en el formulario de inicio de sesión con SweetAlert2

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,7 +1,7 @@
 import { UserModalForm } from "../components/UserModalForm";
 import { UsersList } from "../components/UsersList";
 import { useUsers } from "../hooks/useUsers";
-import "./styles.css";
+import "../styles.css";
 
 export const UsersPage = () => {
 
