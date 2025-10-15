a6d1ab3e0f64aa0c4dbe3c5fd17be6916289a45b
danifer24 <danielvf24@gmail.com>
2025-10-15 16:02:50 +0200

    Eliminar lógica de autenticación del componente UsersApp y utilizar el hook useAuth para manejar el estado de inicio de sesión

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,41 +1,11 @@
-import { useReducer } from "react";
 import { LoginPage } from "./auth/pages/LoginPage";
 import { UsersPage } from "./pages/UsersPage";
-import { loginReducer } from "./auth/pages/reducers/loginReducers";
-import Swal from "sweetalert2";
 import { Navbar } from "./components/layout/Navbar";
-
-const initialLogin = JSON.parse(sessionStorage.getItem("login")) || {
-    isAuth: false,
-    user: undefined,
-}
+import { useAuth } from "./auth/hooks/useAuth";
 
 export const UsersApp = () => {
 
-    const [login, dispatch] = useReducer(loginReducer, initialLogin);
-    const handlerLogin = ({ username, password }) => {
-        if (username === 'admin' || password === '12345') {
-            const user = { username: "admin" };
-            dispatch({
-                type: "login",
-                payload: user,
-            });
-            sessionStorage.setItem("login", JSON.stringify({
-                isAuth: true,
-                user,
-            }))
-
-        } else {
-            Swal.fire("Error de autenticación", "Credenciales incorrectas", "error");
-        }
-    }
-
-    const handlerLogout = () => {
-        dispatch({
-            type: "logout",
-        });
-        sessionStorage.removeItem("login");
-    }
+    const { login, handlerLogin, handlerLogout} = useAuth();
 
     return (
         <>
